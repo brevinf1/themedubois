c86c0ff84cc7d0449263185f00c12dcbb1720852
vladmedenica <vlad.medenica@gmail.com>
2021-02-20 18:57:09 -0500

    Adjust margins

--- R/theme_dubois.R
+++ R/theme_dubois.R
@@ -8,7 +8,7 @@
 theme_dubois <- function() {
 
   theme(text = element_text(family = "Jefferies"),
-        plot.title = element_text(size=20,
+        plot.title = element_text(size=18,
                                   hjust = 0.5,
                                   face = "plain",
                                   margin = margin(0, 0, 1, 0, "cm")),
@@ -16,7 +16,7 @@ theme_dubois <- function() {
                                      hjust = 0.5,
                                      face = "plain"),
         plot.background = element_rect(fill = "#e4d2c1"),
-        plot.margin = margin(1, 1.5, 1, 1.5, "cm"),
+        plot.margin = margin(0.5, 2.25, 0.5, 2.25, "cm"),
         panel.background = element_rect(fill = "#e4d2c1",
                                         color = "#e4d2c1"),
         panel.border = element_rect(color = "#dda58c",
@@ -25,7 +25,7 @@ theme_dubois <- function() {
         panel.grid.minor = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_text(color = "black"),
-        axis.title.x = element_text(margin = margin(0.5, 0, 0.5, 0, "cm")),
+        axis.title.x = element_text(margin = margin(0.5, 0, 0, 0, "cm")),
         legend.background = element_rect(fill = "#e4d2c1"),
         legend.position ="bottom",
         legend.margin = margin(0, 0, 0, 0),
