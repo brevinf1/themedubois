ccab1c014c97f4b45a16502ad98c1657420b9f5c
vladmedenica <vlad.medenica@gmail.com>
2021-02-21 08:08:47 -0500

    Fill short palette

--- R/scale_fill_dubois2.R
+++ R/scale_fill_dubois2.R
@@ -1,6 +1,13 @@
+#' Short color palette (fill)
+#'
+#' @return Fill with short color palette
+#' @export
+#'
+#'
 colors2 <- c("#e23653",
-            "#fcb800",
-            "#577565")
+             "#fcb800",
+             "#577565")
+
 
 scale_fill_dubois2 <- function(){
   scale_fill_manual(values = rev(colors2), guide = guide_legend(reverse = TRUE))
