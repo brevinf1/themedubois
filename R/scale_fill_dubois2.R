54b81c0d5d606f3a72e1e6b6bbfc19133975b521
vladmedenica <vlad.medenica@gmail.com>
2021-02-21 08:13:48 -0500

    fix function

--- R/scale_fill_dubois2.R
+++ R/scale_fill_dubois2.R
@@ -4,11 +4,9 @@
 #' @export
 #'
 #'
-colors2 <- c("#e23653",
-             "#fcb800",
-             "#577565")
-
-
 scale_fill_dubois2 <- function(){
+  colors2 <- c("#e23653",
+               "#fcb800",
+               "#577565")
   scale_fill_manual(values = rev(colors2), guide = guide_legend(reverse = TRUE))
 }
